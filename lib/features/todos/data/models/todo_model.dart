54f7bc620a2da6868e7a001daf365d9415dc418c
Piotr Ekert <piotrekert90@gmail.com>
2026-06-25 09:42:00 +0200

    feat(todos): add Isar todo model and synchronous mapper

--- lib/features/todos/data/models/todo_model.dart
+++ lib/features/todos/data/models/todo_model.dart
@@ -1,7 +1,5 @@
 import 'package:isar_community/isar.dart';
 
-import 'category_model.dart';
-
 part 'todo_model.g.dart';
 
 @collection
@@ -13,7 +11,4 @@ class TodoModel {
   bool isCompleted = false;
 
   late DateTime createdAt;
-
-  /// Optional link to a category. Load with `await category.load()` before reading `.value`.
-  final category = IsarLink<CategoryModel>();
 }
